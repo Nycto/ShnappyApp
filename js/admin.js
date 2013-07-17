70261301d21221c8dc87000fadc745606f99576d
Nycto <James@RoundEights.com>
2013-07-16 22:07:40 -0700

    Embed logged in user in HTML

--- js/admin.js
+++ js/admin.js
@@ -6,26 +6,28 @@ $(window.document).on('click', '.login-button', function (e) {
 });
 
 /** Handle logins via Mozilla Persona */
-navigator.id.watch({
-    loggedInUser: null,
-    onlogin: function(assertion) {
-        $.ajax({
-            type: 'POST',
-            url: '/admin/api/login',
-            data: {assertion: assertion},
-        }).done(function () {
-            window.location.reload();
-        }).fail(function () {
-            navigator.id.logout();
-        });
-    },
-    onlogout: function() {
-        $.ajax({
-            type: 'POST',
-            url: '/admin/api/logout'
-        }).always(function () {
-            window.location.reload();
-        });
-    }
-});
+if ( window.loggedIn !== undefined ) {
+    navigator.id.watch({
+        loggedInUser: window.loggedIn,
+        onlogin: function(assertion) {
+            $.ajax({
+                type: 'PUT',
+                url: '/admin/api/login',
+                data: {assertion: assertion},
+            }).done(function () {
+                window.location.reload();
+            }).fail(function () {
+                navigator.id.logout();
+            });
+        },
+        onlogout: function() {
+            $.ajax({
+                type: 'PUT',
+                url: '/admin/api/logout'
+            }).always(function () {
+                window.location.reload();
+            });
+        }
+    });
+}
 
